ce678385f79c3eaa37556e463bc3c160c1636f48
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-03-20 18:48:47 -0400

    agent loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -31,40 +32,53 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-            temperature=0,
-        ),
-    )
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+                temperature=0,
+            ),
+        )
 
-    usage_metadata = response.usage_metadata
-    if usage_metadata is None:
-        raise RuntimeError("Failed API request: 'usage_metadata' empty.")
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate.content:
+                    messages.append(candidate.content)
 
-    if args.verbose:
-        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {usage_metadata.candidates_token_count}")
-    print("Response:")
-    function_results = list()
-    if response.function_calls:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, args.verbose)
-            if not function_call_result.parts:
-                raise Exception("function call result has no parts")
-            if not function_call_result.parts[0].function_response:
-                raise Exception("function call result isn't a FunctionResponse")
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("function call has no valid response")
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
+        usage_metadata = response.usage_metadata
+        if usage_metadata is None:
+            raise RuntimeError("Failed API request: 'usage_metadata' empty.")
 
+        if args.verbose:
+            print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {usage_metadata.candidates_token_count}")
+        print("Response:")
+        function_responses = list()
+        if response.function_calls:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, args.verbose)
+                if not function_call_result.parts:
+                    raise Exception("function call result has no parts")
+                if not function_call_result.parts[0].function_response:
+                    raise Exception("function call result isn't a FunctionResponse")
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception("function call has no valid response")
+                function_responses.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(
+                        f"-> {function_call_result.parts[0].function_response.response}"
+                    )
+            messages.append(types.Content(role="user", parts=function_responses))
+
+        else:
+            print(response.text)
+            return
     else:
-        print(response.text)
+        print("Error: Maximum number of iterations reached without success.")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
